aa4f324c97d955e6da97bb450d783ec66ee5a045
Anuj Singh <anuj60361@gmail.com>
2021-06-21 01:16:58 +0530

    Update RabbitMQ

--- RabbitMQ-Produce&Consume-Java-POJOs/rabbitmqConsumer/src/main/java/com/example/demo/config/MessagingConfig.java
+++ RabbitMQ-Produce&Consume-Java-POJOs/rabbitmqConsumer/src/main/java/com/example/demo/config/MessagingConfig.java
@@ -1,43 +1,14 @@
 package com.example.demo.config;
 
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.example.demo.util.QueueConstants;
-
 @Configuration
 public class MessagingConfig {
-
-	@Bean
-	public Queue getMyQueue() {
-		return new Queue(QueueConstants.MY_Queue);
-	}
-	
-	@Bean
-	public TopicExchange getTopicExchange() {
-		return ExchangeBuilder
-				.topicExchange(QueueConstants.EXCHANGE)
-				.durable(true)
-				.build();
-	}
-	
-	@Bean
-	public Binding getBinding() {
-		return BindingBuilder.bind(getMyQueue())
-				.to(getTopicExchange())
-				.with(QueueConstants.ROUTING_KEY);
-	}
 	
 	@Bean
 	public MessageConverter converter() {
@@ -50,13 +21,5 @@ public class MessagingConfig {
 		cachingConnectionFactory.setUsername("guest");
 		cachingConnectionFactory.setPassword("guest");
 		return cachingConnectionFactory;
-	}
-	
-	@Bean
-	public AmqpTemplate tempate() {
-		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-		rabbitTemplate.setMessageConverter(converter());
-		return rabbitTemplate;
-	}
-	
+	}	
 }
